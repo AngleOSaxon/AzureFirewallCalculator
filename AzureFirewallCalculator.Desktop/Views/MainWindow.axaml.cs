dd1a844f43346f41cd5ba5e4544292e88adbb971
Andrew <andrewbikle@gmail.com>
2023-10-15 14:15:05 -0400

    Debounce scroll-to-end to make sure it scrolls only after big batches are finished processing

--- AzureFirewallCalculator.Desktop/Views/MainWindow.axaml.cs
+++ AzureFirewallCalculator.Desktop/Views/MainWindow.axaml.cs
@@ -1,8 +1,11 @@
-using System.Linq;
+using System;
+using System.Collections.Specialized;
+using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
 using Avalonia.VisualTree;
 using AzureFirewallCalculator.Desktop.ViewModels;
 using ReactiveUI;
@@ -19,11 +22,19 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         this.AttachDevTools();
 #endif
         var logView = this.FindControl<ListBox>("LogView");
+
         // Null derefs can and should crash us, because should never be null
-        logView!.Items.CollectionChanged += (sender, e) =>
-        {
-            var scrollViewer = logView!.FindDescendantOfType<ScrollViewer>();
-            scrollViewer!.ScrollToEnd();
-        };
+        // TODO: Review to make sure is disposed properly and doesn't leak
+        // Though this view is currently expected to live for the lifetime of the app
+        _ = Observable.FromEventPattern<NotifyCollectionChangedEventArgs>(logView!.Items, nameof(logView.Items.CollectionChanged)) // TODO: Can I do this with a direct reference and not a string?
+            .Throttle(TimeSpan.FromMilliseconds(500))
+            .Subscribe(e =>
+            {
+                Dispatcher.UIThread.Invoke(() =>
+                {
+                    var scrollViewer = logView!.FindDescendantOfType<ScrollViewer>();
+                    scrollViewer!.ScrollToEnd();
+                });
+            });
     }
 }
